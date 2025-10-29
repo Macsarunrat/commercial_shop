3756effbacd966b4c2775450b468d97f10b8b5f2
Sarunrat <tee20122538@gmail.com>
2025-10-29 21:46:28 +0700

    Merge main.py and database.py from feature/product

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
 from fastapi import FastAPI
+from database import create_db_and_tables
+from api import products,catagory,brand,image
+import os
 
-app = FastAPI()
+app = FastAPI(title="Zhopee")
+
+app.include_router(products.router)
+app.include_router(catagory.router)
+app.include_router(brand.router)
+app.include_router(image.router)
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
