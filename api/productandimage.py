97d0a1a570a379393d717e7b70de0c766756bfa3
Sarunrat <tee20122538@gmail.com>
2025-10-31 23:15:40 +0700

    omit error

--- api/productandimage.py
+++ api/productandimage.py
@@ -1,16 +1,16 @@
-from fastapi import APIRouter, Depends
-from sqlmodel import Session
-from typing import Annotated,List
-from database import get_session
-import crud.productandimage as crud_productandimage
+# from fastapi import APIRouter, Depends
+# from sqlmodel import Session
+# from typing import Annotated,List
+# from database import get_session
+# import crud.productandimage as crud_productandimage
 
-SessionDep = Annotated[Session,Depends(get_session)]
+# SessionDep = Annotated[Session,Depends(get_session)]
 
-router = APIRouter(
-    prefix= "/product-image",
-    tags = ["product-image"]
-)
+# router = APIRouter(
+#     prefix= "/product-image",
+#     tags = ["product-image"]
+# )
 
-@router.get("/{shop_id}/product-image")
-def read_product_image_by_shop(session : SessionDep, shop_id: int):
-    return crud_productandimage.read_product_image_by_shop(session,shop_id)
+# @router.get("/{shop_id}/product-image")
+# def read_product_image_by_shop(session : SessionDep, shop_id: int):
+#     return crud_productandimage.read_product_image_by_shop(session,shop_id)
